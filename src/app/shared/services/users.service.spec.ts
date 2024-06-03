b618ea09a5cd1e8a89598f9fbb03978acf5425aa
molchanovchess <alexmolchanovchess@gmail.com>
2024-06-03 17:08:48 +0300

    testing rxjs streams

--- src/app/shared/services/users.service.spec.ts
+++ src/app/shared/services/users.service.spec.ts
@@ -1,25 +1,24 @@
 import { UserInterface } from "../types/user.interface";
 import { UsersService } from "./users.service";
 import { TestBed } from "@angular/core/testing"
-import { UtilsService } from "./utils.service";
 
 describe('UserService', () => {
     let userService: UsersService;
-    let utilsService: UtilsService;
+    // let utilsService: UtilsService;
     // const utilsServiceMock = {
     //     pluck: jest.fn(),
     // }
     beforeEach(() => {
         TestBed.configureTestingModule({
             providers: [
-                UsersService,
-                UtilsService
+                UsersService
+                // UtilsService
                 // {provide: UtilsService, useValue: utilsServiceMock}
             ],
         });
 
         userService = TestBed.inject(UsersService);
-        utilsService = TestBed.inject(UtilsService);
+        // utilsService = TestBed.inject(UtilsService);
     });
 
     it('creates a service', () => {
@@ -33,29 +32,32 @@ describe('UserService', () => {
                 name: 'foo'
             }
             userService.addUser(user)
-            expect(userService.users).toEqual([{id: '3', name: 'foo'}]);
+            // expect(userService.users).toEqual([{id: '3', name: 'foo'}]);
+            expect(userService.users$.getValue()).toEqual([{id: '3', name: 'foo'}]);
         });
     });
     describe('removeUser', () => {
         it('should remove a user', () => {
-            userService.users = [{id: '3', name: 'foo'}];
+            // userService.users = [{id: '3', name: 'foo'}];
+            userService.users$.next([{id: '3', name: 'foo'}]);
             userService.removeUser('3');
-            expect(userService.users).toEqual([]);
+            // expect(userService.users).toEqual([]);
+            expect(userService.users$.getValue()).toEqual([]);
         });
 
     });
-    describe('getUsernames', () => {
-        it('should get usernames', () => {
-            jest.spyOn(utilsService, 'pluck');
-            userService.users = [{id: '3', name: 'foo'}];
-            userService.getUsernames();
-            expect(utilsService.pluck).toHaveBeenCalledWith(
-                userService.users,
-                'name'
-            )
+    // describe('getUsernames', () => {
+    //     it('should get usernames', () => {
+    //         jest.spyOn(utilsService, 'pluck');
+    //         userService.users = [{id: '3', name: 'foo'}];
+    //         userService.getUsernames();
+    //         expect(utilsService.pluck).toHaveBeenCalledWith(
+    //             userService.users,
+    //             'name'
+    //         )
 
-            // utilsServiceMock.pluck.mockReturnValue(['foo']);
-            // expect(userService.getUsernames()).toEqual(['foo']);
-        })
-    })
+    //         // utilsServiceMock.pluck.mockReturnValue(['foo']);
+    //         // expect(userService.getUsernames()).toEqual(['foo']);
+    //     })
+    // })
 });
--- src/app/shared/services/users.service.ts
+++ src/app/shared/services/users.service.ts
@@ -1,22 +1,30 @@
-import { Injectable, inject } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { UserInterface } from "../types/user.interface";
-import { UtilsService } from "./utils.service";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable()
 export class UsersService {
-    utilsService = inject(UtilsService);
-    users: UserInterface[] = [];
+    // utilsService = inject(UtilsService);
+    // users: UserInterface[] = [];
+    users$ = new BehaviorSubject<UserInterface[]>([])
 
     addUser(user: UserInterface): void {
-        this.users = [...this.users, user]
+        // this.users = [...this.users, user]
+
+        this.users$.next([...this.users$.getValue(), user])
     }
 
     removeUser(userId: string): void {
-        const updatedUsers = this.users.filter((user) => userId !== user.id);
-        this.users = updatedUsers;
+        // const updatedUsers = this.users.filter((user) => userId !== user.id);
+        // this.users = updatedUsers;
+
+        const updatedUsers = this.users$
+            .getValue()
+            .filter((user) => userId !== user.id);
+        this.users$.next(updatedUsers);
     }
 
-    getUsernames(): string[] {
-        return this.utilsService.pluck(this.users, 'name');
-    }
+    // getUsernames(): string[] {
+    //     return this.utilsService.pluck(this.users, 'name');
+    // }
 }
